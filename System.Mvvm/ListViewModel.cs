a780b96e089df11b006bb56216d3d51fbf0cc722
newky2k <newky2k@noreply.github.com>
2019-06-11 16:00:03 +0100

    Added list and search view models

---
+++ System.Mvvm/ListViewModel.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace System.Mvvm
+{
+    public abstract class ListViewModel<T, T2> : ViewModel where T2 : IEnumerable<T>, new()
+    {
+        #region Fields
+        private T selectedItem;
+        protected int itemCount;
+        private T2 items;
+        private T2 selectedItems;
+        #endregion
+
+        #region Events
+
+        public event EventHandler<T> SelectedItemChanged = delegate { };
+
+        public event EventHandler OnDataRefreshed = delegate { };
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the selected items.
+        /// </summary>
+        /// <value>The selected items.</value>
+        public T2 SelectedItems
+        {
+            get
+            {
+                return selectedItems;
+            }
+            set
+            {
+                selectedItems = value;
+                NotifyPropertyChanged("SelectedItems");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the items.
+        /// </summary>
+        /// <value>
+        /// The items.
+        /// </value>
+        public virtual T2 Items
+        {
+            get
+            {
+                if (items == null)
+                    items = new T2();
+
+                itemCount = items.Count();
+
+                return items;
+
+            }
+            set
+            {
+                items = value;
+
+                NotifyPropertyChanged("Items");
+                NotifyPropertyChanged("ItemCount");
+            }
+        }
+
+        public string ItemCount
+        {
+            get
+            {
+                var msg = (itemCount == 1) ? "item" : "items";
+
+                return String.Format("Found {0} {1}", itemCount, msg);
+            }
+        }
+
+        /// <summary>
+        /// Gets the refresh command.
+        /// </summary>
+        /// <value>
+        /// The refresh command.
+        /// </value>
+        public virtual ICommand RefreshCommand
+        {
+            get
+            {
+                return new DelegateCommand(async () =>
+                {
+                    await RefreshAsync();
+                });
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the selected item.
+        /// </summary>
+        /// <value>The selected item.</value>
+        public virtual T SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                NotifyPropertyChanged("SelectedItem");
+                SelectedItemChanged(this, value);
+                HandleItemSelection(value);
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseSearchViewModel"/> class.
+        /// </summary>
+        /// <param name="searchText">The search text.</param>
+        public ListViewModel()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Reloads the data asynchronously
+        /// </summary>
+        /// <returns>Task.</returns>
+        public abstract Task RefreshAsync();
+
+        public void DidRefreshData()
+        {
+            OnDataRefreshed(this, new EventArgs());
+
+        }
+
+        protected virtual void HandleItemSelection(T selection)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ System.Mvvm/SearchViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace System.Mvvm
+{
+    public abstract class SearchViewModel<T, T2> : ListViewModel<T,T2> where T2 : IEnumerable<T>, new()
+    {
+        #region Fields
+        private String searchText;
+        #endregion
+
+        #region Events
+
+        public event EventHandler<T> ItemDoubleClicked = delegate { };
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the items.
+        /// </summary>
+        /// <value>
+        /// The items.
+        /// </value>
+        public override T2 Items
+        {
+            get
+            {
+                var results = ApplyFilter(base.Items);
+
+                itemCount = results.Count();
+
+                return results;
+
+            }
+            set
+            {
+                base.Items = value;
+
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the search text.
+        /// </summary>
+        /// <value>
+        /// The search text.
+        /// </value>
+        public String SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+
+                NotifyPropertyChanged("SearchText");
+                NotifyPropertyChanged("Items");
+                NotifyPropertyChanged("ItemCount");
+            }
+        }
+
+        /// <summary>
+        /// Gets the clear search command.
+        /// </summary>
+        /// <value>
+        /// The clear search command.
+        /// </value>
+        public ICommand ClearSearchCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    SearchText = null;
+                    SelectedItem = default(T);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Gets the refresh command.
+        /// </summary>
+        /// <value>
+        /// The refresh command.
+        /// </value>
+        public override ICommand RefreshCommand
+        {
+            get
+            {
+                return new DelegateCommand(async () =>
+                {
+                    SearchText = null;
+
+                    await RefreshAsync();
+                });
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseSearchViewModel"/> class.
+        /// </summary>
+        /// <param name="searchText">The search text.</param>
+        public SearchViewModel(string searchText = null)
+        {
+            this.searchText = searchText;
+        }
+        #endregion
+
+        #region Methods
+
+        protected abstract T2 ApplyFilter(T2 data);
+
+        public virtual void DidDoubleClick(T item)
+        {
+            ItemDoubleClicked(this, item);
+        }
+
+
+        #endregion
+    }
+}
