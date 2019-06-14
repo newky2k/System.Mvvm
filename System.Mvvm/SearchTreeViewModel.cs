8f7630c1a39804d3251b9ac0d3daadf9728b757c
newky2k <newky2k@noreply.github.com>
2019-06-14 14:16:20 +0100

    Added searchable tree view model

---
+++ System.Mvvm/SearchTreeViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Mvvm
+{
+    public abstract class SearchTreeViewModel<T, T2> : SearchViewModel<T, List<T>>
+    {
+
+        #region Properties
+
+        public IReadOnlyCollection<T2> TreePath
+        {
+            get
+            {
+
+                NotifyPropertyChanged("ItemCount");
+
+                return BuildTreePath(Items);
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseSearchViewModel"/> class.
+        /// </summary>
+        /// <param name="searchText">The search text.</param>
+        public SearchTreeViewModel(string searchText = null) : base(searchText)
+        {
+
+        }
+        #endregion
+
+        #region Methods
+
+        protected abstract IReadOnlyCollection<T2> BuildTreePath(List<T> data);
+
+
+        #endregion
+    }
+}
