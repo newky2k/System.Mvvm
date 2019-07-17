05f7013c5fb9218b7fb7ccbd686c21d7f81d99c2
newky2k <newky2k@noreply.github.com>
2019-07-17 15:28:12 +0100

    Added sample wpf application

---
+++ MVVMSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Mvvm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        public MainViewModel() : base()
+        {
+
+        }
+    }
+}
