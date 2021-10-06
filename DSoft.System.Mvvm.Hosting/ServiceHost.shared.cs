794dcaec9f5bdec35009f74851248ec012dabe89
David Humphreys <davidh@westwoodrock.onmicrosoft.com>
2021-10-06 20:50:32 +0100

    Added sceondary host

--- DSoft.System.Mvvm.Hosting/ServiceHost.shared.cs
+++ DSoft.System.Mvvm.Hosting/ServiceHost.shared.cs
@@ -12,30 +12,54 @@ namespace System.Mvvm
     public static partial class ServiceHost
     {
         private static IHost _host;
+        private static IHost _secondaryHost;
 
         /// <summary>
-        /// Gets or sets the gloabl IHost instance
+        /// Gets or sets the primary global IHost instance
         /// </summary>
         /// <value>
-        /// The host.
+        /// The primary host.
         /// </value>
         /// <exception cref="System.NullReferenceException">The Host property has not been set on ServiceHost</exception>
         public static IHost Host
         {
-            get 
+            get
             {
                 if (_host == null)
                     throw new NullReferenceException("The Host property has not been set on ServiceHost");
 
 
-                return _host; 
+                return _host;
             }
             set { _host = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the primary global IHost instance
+        /// </summary>
+        /// <value>
+        /// The secondary host.
+        /// </value>
+        /// <exception cref="System.NullReferenceException">The SecondaryHost property has not been set on ServiceHost</exception>
+        public static IHost SecondaryHost
+        {
+            get
+            {
+                if (_secondaryHost == null)
+                    throw new NullReferenceException($"The SecondaryHost property has not been set on ServiceHost");
+
+                return _secondaryHost;
+            }
+            set { _secondaryHost = value; }
+        }
+
         public static IServiceProvider Provider => Host.Services;
 
+        public static IServiceProvider SecondaryProvider => SecondaryHost.Services;
+
         public static IServiceScope Scope => Provider.CreateScope();
+
+        public static IServiceScope SecondaryScope => SecondaryProvider.CreateScope();
 
         /// <summary>
         /// Gets the required service with an existing scope
@@ -45,33 +69,53 @@ namespace System.Mvvm
         /// <returns></returns>
         public static T GetRequiredService<T>(IServiceScope scope) => scope.ServiceProvider.GetRequiredService<T>();
 
+
         /// <summary>
         /// Gets the required service with a new scope
         /// </summary>
-        /// <typeparam name="T">Service Type</typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="hostMode">The host to use.</param>
         /// <returns></returns>
-        public static T GetRequiredService<T>() => Provider.CreateScope().ServiceProvider.GetRequiredService<T>();
+        public static T GetRequiredService<T>(HostMode hostMode = HostMode.Primary)
+        {
+            if (hostMode == HostMode.Primary)
+                return Provider.CreateScope().ServiceProvider.GetRequiredService<T>();
+            else
+                return SecondaryProvider.CreateScope().ServiceProvider.GetRequiredService<T>();
+        }
 
         /// <summary>
         /// Starts the host asynchronously.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        public static Task StartAsync(CancellationToken cancellationToken = default) => Host.StartAsync(cancellationToken);
+        public static Task StartAsync(HostMode hostMode = HostMode.Primary, CancellationToken cancellationToken = default)
+        {
+            if (hostMode == HostMode.Primary)
+                return Host.StartAsync(cancellationToken);
+            else
+                return SecondaryHost.StartAsync(cancellationToken);
+        }
 
         /// <summary>
         /// Stops the host asynchronously.
         /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns></returns>
-        public static Task StopAsync(CancellationToken cancellationToken = default) => Host.StopAsync(cancellationToken);
+        public static Task StopAsync(HostMode hostMode = HostMode.Primary, CancellationToken cancellationToken = default)
+        {
+            if (hostMode == HostMode.Primary)
+                return Host.StopAsync(cancellationToken);
+            else
+                return SecondaryHost.StopAsync(cancellationToken);
+        }
 
         /// <summary>
         /// Initializes the ServiceHost
         /// </summary>
         /// <param name="hostConfigurationBuilder">The host configuration builder.</param>
         /// <param name="serviceConfigurationAction">The service configuration action.</param>
-        public static IHost Initialize(Action<IConfigurationBuilder> hostConfigurationBuilder, Action<HostBuilderContext, IServiceCollection> servicesConfigurationAction)
+        public static IHost Initialize(Action<IConfigurationBuilder> hostConfigurationBuilder, Action<HostBuilderContext, IServiceCollection> servicesConfigurationAction, HostMode hostMode = HostMode.Primary)
         {
 
             IHostBuilder builder = new HostBuilder();
@@ -82,9 +126,14 @@ namespace System.Mvvm
             if (servicesConfigurationAction != null)
                 builder = builder.ConfigureServices(servicesConfigurationAction);
 
-            Host = builder.Build();
+            var aHost = builder.Build();
 
-            return Host;
+            if (hostMode == HostMode.Primary)
+                Host = aHost;
+            else
+                SecondaryHost = aHost;
+
+            return aHost;
         }
 
         /// <summary>
@@ -92,17 +141,22 @@ namespace System.Mvvm
         /// </summary>
         /// <param name="rootPath">The root path.</param>
         /// <param name="serviceConfigurationAction">The service configuration action.</param>
-        public static IHost Initialize(string rootPath, Action<HostBuilderContext, IServiceCollection> servicesConfigurationAction) => Initialize((c) => { c.AddCommandLine(new string[] { $"ContentRoot={rootPath}" }); }, servicesConfigurationAction);
+        public static IHost Initialize(string rootPath, Action<HostBuilderContext, IServiceCollection> servicesConfigurationAction, HostMode hostMode = HostMode.Primary) => Initialize((c) => { c.AddCommandLine(new string[] { $"ContentRoot={rootPath}" }); }, servicesConfigurationAction, hostMode);
 
         /// <summary>
         /// Initializes the ServiceHost
         /// </summary>
         /// <param name="serviceConfigurationAction">The service configuration action.</param>
-       public static IHost Initialize(Action<HostBuilderContext, IServiceCollection> configAction)
+       public static IHost Initialize(Action<HostBuilderContext, IServiceCollection> configAction, HostMode hostMode = HostMode.Primary)
         {
-            Host = new HostBuilder().ConfigureServices(configAction).Build();
+            var aHost = new HostBuilder().ConfigureServices(configAction).Build();
 
-            return Host;
+            if (hostMode == HostMode.Primary)
+                Host = aHost;
+            else
+                SecondaryHost = aHost;
+
+            return aHost;
         }
 
 
