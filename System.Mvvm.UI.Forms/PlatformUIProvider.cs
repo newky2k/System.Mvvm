b0f4745e962654fd0e4c4a15f2e14ae77862fa6e
David Humphreys <davidh@westwoodrock.onmicrosoft.com>
2021-11-25 16:46:49 +0000

    Added support for Xamarin forms and using essentials

--- System.Mvvm.UI.Forms/PlatformUIProvider.cs
+++ System.Mvvm.UI.Forms/PlatformUIProvider.cs
@@ -13,96 +13,13 @@ namespace System.Mvvm
     {
         public Application CurrentApplication => Application.Current;
 
-        /// <summary>
-        /// Gets or sets the show alert overide function.
-        /// </summary>
-        /// <value>
-        /// The show alert overide function.
-        /// </value>
-        public Func<string, string, Task> ShowAlertOverideFunction { get; set; }
+        public Task ShowAlertAsync(string title, string message) => CurrentApplication.MainPage.DisplayAlert(title, message, "OK");
 
-        /// <summary>
-        /// Gets or sets the show confirm overide function.
-        /// </summary>
-        /// <value>
-        /// The show confirm overide function.
-        /// </value>
-        public Func<string, string, Task<bool>> ShowConfirmOverideFunction { get; set; }
+        public Task<bool> ShowConfirmationDialogAsync(string title, string message) => CurrentApplication.MainPage.DisplayAlert(title, message, "Ok", "Cancel");
 
-        public void InvokeOnUIThread(Action action)
-        {
-            //if (CurrentDispatcher.CheckAccess())
-            //{
-            //    action();
-            //}
-            //else
-            //{
-            //    CurrentDispatcher.Invoke(action);
-            //}
-        }
+        public void InvokeOnUIThread(Action action) => Xamarin.Essentials.MainThread.BeginInvokeOnMainThread(action);
 
-        public Task InvokeOnUIThreadAsync(Action action)
-        {
-            return Task.FromResult(true);
-
-            //if (CurrentDispatcher.CheckAccess())
-            //{
-            //    action();
-
-            //    return Task.CompletedTask;
-            //}
-            //else
-            //{
-            //    return CurrentDispatcher.InvokeAsync(action).Task;
-            //}
-                
-        }
-
-        public async Task ShowAlertAsync(string title, string message)
-        {
-            if (ShowAlertOverideFunction != null)
-            {
-                await InvokeOnUIThreadAsync(async () =>
-                {
-                    await ShowAlertOverideFunction(title, message);
-
-                });
-            }
-            else
-            {
-                await InvokeOnUIThreadAsync(() =>
-                {
-                    //MessageBox.Show(message, title);
-                });
-            }
-
-
-        }
-
-        public async Task<bool> ShowConfirmationDialogAsync(string title, string message)
-        {
-            //var tsk = new TaskCompletionSource<bool>();
-            var result = false;
-
-            if (ShowConfirmOverideFunction != null)
-            {
-                return await ShowConfirmOverideFunction(title, message);
-            }
-            else
-            {
-                await InvokeOnUIThreadAsync(() =>
-                {
-                    //var confirm = MessageBox.Show(message, title, MessageBoxButton.YesNo);
-
-                    //result = (confirm == MessageBoxResult.Yes);
-                });
-            }
-
-                
-
-            return result;   
-        }
-
+        public Task InvokeOnUIThreadAsync(Action action) => Xamarin.Essentials.MainThread.InvokeOnMainThreadAsync(action);
 
     }
 
