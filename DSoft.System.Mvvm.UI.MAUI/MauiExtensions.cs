efefc193b35f4756044b2aecbc28624e538866a4
David H <newky2k@users.noreply.github.com>
2022-08-14 21:07:02 +0100

    Added MAUI support and MAUI samples

---
+++ DSoft.System.Mvvm.UI.MAUI/MauiExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Maui.Hosting
+{
+	public static class MauiExtensions
+	{
+		public static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder, Action<IServiceCollection> configureDelegate)
+		{
+			configureDelegate.Invoke(builder.Services);
+
+			return builder;
+		}
+	}
+}
